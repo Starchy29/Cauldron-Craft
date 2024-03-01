df33089aa4f4737df50a9c3f12afcbaad153887c
Ethan Ball (RIT Student) <ejb3432@rit.edu>
2024-03-01 17:20:21 -0500

    added cauldron code

--- House Haunters/Assets/Scripts/Level/Cauldron.cs
+++ House Haunters/Assets/Scripts/Level/Cauldron.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Cauldron : GridEntity
 {
-    private MonsterType cookingMonster;
+    private MonsterName? cookingMonster;
     private bool cookingTurn;
 
     void Start() {
@@ -12,9 +12,20 @@ public class Cauldron : GridEntity
     }
 
     private void Cook() {
-        if(cookingMonster != null) {
+        if(cookingMonster.HasValue) {
             // find the spot to spawn on
+            LevelGrid level = LevelGrid.Instance;
+            Vector2Int levelMid = new Vector2Int(level.Width / 2, level.Height / 2);
+            List<Vector2Int> options = level.GetTilesInRange(Tile, 1, true).Filter((Vector2Int tile) => { return level.GetEntity(tile) == null; });
+            if(options.Count == 0) {
+                return;
+            }
 
+            options.Sort((Vector2Int current, Vector2Int next) => { return Global.CalcTileDistance(current, levelMid) - Global.CalcTileDistance(next, levelMid); });
+            Vector2Int spawnSpot = options[0];
+
+            // spawn the monster
+            GameManager.Instance.SpawnMonster(cookingMonster.Value, spawnSpot, Controller);
             cookingMonster = null;
         }
     }
--- House Haunters/Assets/Scripts/UI/BuyMonsterButton.cs
+++ House Haunters/Assets/Scripts/UI/BuyMonsterButton.cs
@@ -8,16 +8,17 @@ public class BuyMonsterButton : AutoButton
     [SerializeField] private SpriteRenderer monsterImage;
     [SerializeField] private SpriteRenderer[] ingredientLogos;
 
-    public MonsterType MonsterOption { get; private set; }
+    public MonsterName MonsterOption { get; private set; }
 
     public void SetMonster(MonsterName monster) {
-        MonsterOption = MonstersData.Instance.GetMonsterData(monster);
+        MonsterOption = monster;
+        MonsterType data = MonstersData.Instance.GetMonsterData(monster);
 
         monsterImage.sprite = PrefabContainer.Instance.monsterToSprite[monster];
 
         int nextIngredient = 0;
         foreach(Ingredient ingredient in Enum.GetValues(typeof(Ingredient))) {
-            for(int i = 0; i < MonsterOption.Recipe[ingredient]; i++) {
+            for(int i = 0; i < data.Recipe[ingredient]; i++) {
                 ingredientLogos[nextIngredient].sprite = PrefabContainer.Instance.ingredientToSprite[ingredient];
                 nextIngredient++;
             }

c0ac2e5662918b6d44e9ae3b502584ebeebfb6eb
Starchy29 <ethanball29@gmail.com>
2024-04-15 23:48:14 -0400

    added health indicator to monster menu

--- House Haunters/Assets/Scripts/UI/MoveMenu.cs
+++ House Haunters/Assets/Scripts/UI/MoveMenu.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoveMenu : MonoBehaviour
 {
     [SerializeField] private GameObject MoveButtonPrefab;
+    [SerializeField] private GameObject MonsterInfoAnchor;
+    [SerializeField] private TextMeshPro HealthMarker;
+    [SerializeField] private SpriteRenderer Heart;
 
     private MoveButton[] buttons;
     private int numButtons;
@@ -30,8 +34,9 @@ public class MoveMenu : MonoBehaviour
         gameObject.SetActive(true);
         Move[] moves = monster.Stats.Moves;
 
+        // set up move buttoms
         numButtons = moves.Length;
-        float buttonSpan = buttonHeight * numButtons + BUTTON_GAP * (numButtons - 1);
+        float buttonSpan = buttonHeight * (numButtons + 1) + BUTTON_GAP * numButtons;
         Background.transform.localScale = new Vector3(MoveButtonPrefab.transform.localScale.x + 0.5f, buttonSpan + 0.5f, 1);
         for(int i = 0; i < buttons.Length; i++) {
             if(i >= numButtons) {
@@ -40,9 +45,27 @@ public class MoveMenu : MonoBehaviour
             }
 
             buttons[i].gameObject.SetActive(true);
-            buttons[i].transform.localPosition = new Vector3(0, -(buttonSpan - buttonHeight) / 2f + i * (buttonHeight + BUTTON_GAP), 0);
+            buttons[i].transform.localPosition = new Vector3(0, -(buttonSpan - buttonHeight) / 2f + (i + 1) * (buttonHeight + BUTTON_GAP), 0);
             buttons[i].SetMove(monster, i);
             buttons[i].disabled = monster.Controller != player || !monster.CanUse(i);
+        }
+
+        // set up monster info
+        MonsterInfoAnchor.transform.localPosition = new Vector3(0, -(buttonSpan - buttonHeight) / 2f, 0);
+        HealthMarker.text = monster.Health + "/" + monster.Stats.Health;
+        float healthPercent = (float)monster.Health / monster.Stats.Health;
+        Heart.color = new Color(0f, 0.6f, 0f);
+        if(healthPercent <= 0.1f) {
+            Heart.color = new Color(0.5f, 0.0f, 0.0f);
+        }
+        else if(healthPercent <= 0.25f) {
+            Heart.color = new Color(0.8f, 0.0f, 0.0f);
+        }
+        else if(healthPercent <= 0.5f) {
+            Heart.color = new Color(0.8f, 0.8f, 0.2f);
+        }
+        else if(healthPercent < 1.0f) {
+            Heart.color = new Color(0.3f, 0.8f, 0.2f);
         }
     }
 }
